0307d86bd6894d721aff206b8ad5314b1a212d0d
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-03-01 20:20:37 +0530

    first commit

---
+++ js/mobileInput.js
@@ -0,0 +1,69 @@
+let touchStartX = 0;
+let touchStartY = 0;
+let touchEndX = 0;
+let touchEndY = 0;
+
+const board = document.querySelector("#game-board");
+
+// Handle touch start event
+board.addEventListener("touchstart", (e) => {
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+
+// Handle touch move event
+board.addEventListener("touchmove", (e) => {
+  touchEndX = e.touches[0].clientX;
+  touchEndY = e.touches[0].clientY;
+});
+
+// Handle touch end event
+board.addEventListener("touchend", () => {
+  var deltaX = touchEndX - touchStartX;
+  var deltaY = touchEndY - touchStartY;
+
+  // Determine if swipe was horizontal or vertical
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    // Horizontal swipe
+    if (deltaX > 0) {
+      // Swipe right
+      const event = new KeyboardEvent("keydown", {
+        key: "ArrowRight",
+        code: "ArrowRight",
+        bubbles: true,
+      });
+      document.dispatchEvent(event);
+      console.log("right");
+    } else if (deltaX < 0) {
+      // Swipe left
+      const event = new KeyboardEvent("keydown", {
+        key: "ArrowLeft",
+        code: "ArrowLeft",
+        bubbles: true,
+      });
+      document.dispatchEvent(event);
+      console.log("left");
+    }
+  } else {
+    // Vertical swipe
+    if (deltaY > 0) {
+      // Swipe down
+      const event = new KeyboardEvent("keydown", {
+        key: "ArrowDown",
+        code: "ArrowDown",
+        bubbles: true,
+      });
+      document.dispatchEvent(event);
+      console.log("down");
+    } else if (deltaY < 0) {
+      // Swipe up
+      const event = new KeyboardEvent("keydown", {
+        key: "ArrowUp",
+        code: "ArrowUp",
+        bubbles: true,
+      });
+      document.dispatchEvent(event);
+      console.log("up");
+    }
+  }
+});
